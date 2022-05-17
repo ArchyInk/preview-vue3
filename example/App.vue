4a61404db438b5eec797604f88f50d1cd89cf33c
archy <archybool@gmail.com>
2022-05-17 10:08:59 +0800

    删除预览文件

--- example/App.vue
+++ example/App.vue
@@ -2,33 +2,27 @@
  * @author: Archy
  * @Date: 2022-04-22 10:03:25
  * @LastEditors: Archy
- * @LastEditTime: 2022-05-13 13:47:37
- * @FilePath: \vue3-preview\example\App.vue
+ * @LastEditTime: 2022-05-17 10:07:48
+ * @FilePath: \preview-vue3\example\App.vue
  * @description: 
 -->
 <script setup lang="ts">
 import { ref } from 'vue'
-const visible = ref(true)
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 </script>
 
 <template>
-  <span v-preview="import('./assets/test.jpg')">正常jpg图片</span>
+  <span v-preview="'http://demo.archy.ink/preview-vue3/assets/test.744b6685.png'">正常jpg图片</span>
   <br />
-  <span v-preview="import('./assets/test.png')">过宽png图片</span>
+  <span v-preview="'http://demo.archy.ink/preview-vue3/assets/test.744b6685.png'">过宽png图片</span>
   <br />
-  <span v-preview="import('./assets/test.pdf')">pdf</span>
+  <span v-preview="'http://demo.archy.ink/preview-vue3/assets/test.1cd30900.pdf'">pdf</span>
   <br />
-  <span v-preview="import('./assets/test.mp3')">音频</span>
+  <span v-preview="'http://demo.archy.ink/preview-vue3/assets/test.5436ebd4.mp3'">音频</span>
   <br />
-  <span v-preview="import('./assets/test.mp4')">视频</span>
-  <br />
-  <span v-preview="'C://img'">未知类型</span>
+  <span v-preview="'http://demo.archy.ink/preview-vue3/assets/test.f98776a7.mp4'">视频</span>
   <br />
   <span v-preview="'C://img'">未知类型</span>
   <br />
-  <!-- <Warpper v-model:visible="visible"></Warpper> -->
 </template>
 
 <style>
